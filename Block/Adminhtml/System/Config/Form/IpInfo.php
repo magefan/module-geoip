ea4b9444ac152a141fc278a3f8624fbcd275a45c
Magefan <ihoronesuch@gmail.com>
2019-01-07 12:14:46 +0200

    Update IpInfo.php

--- Block/Adminhtml/System/Config/Form/IpInfo.php
+++ Block/Adminhtml/System/Config/Form/IpInfo.php
@@ -2,8 +2,6 @@
 /**
  * Copyright © Magefan (support@magefan.com). All rights reserved.
  * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
- *
- * Glory to Ukraine! Glory to the heroes!
  */
 
 namespace Magefan\GeoIp\Block\Adminhtml\System\Config\Form;
